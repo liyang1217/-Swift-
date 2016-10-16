a392a2fc6d769d1903ac66646e1a343aa1de4109
BARCELONA <BARCELONA@dllodeMac-mini-120.local>
2016-10-16 13:32:00 +0800

    初始化工程

---
+++ 斗鱼/斗鱼/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  斗鱼
+//
+//  Created by BARCELONA on 2016/10/16.
+//  Copyright © 2016年 LY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
